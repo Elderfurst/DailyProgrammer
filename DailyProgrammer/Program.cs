8f58d99eacaa63e85cf35b39023e7e6a466204a9
Nick Anderson <nanderson@reliaslearning.com>
2017-10-11 11:17:30 -0400

    Challenge 335 - Easy: https://www.reddit.com/r/dailyprogrammer/comments/759fha/20171009_challenge_335_easy_consecutive_distance/

---
+++ DailyProgrammer/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyProgrammer
+{
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            var runnable = new Easy335();
+            runnable.Run();
+        }
+    }
+}
